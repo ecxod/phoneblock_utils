e73bec65021639ae5a39f9b524d9bb4dbbd3bfd6
Christian Eichert <c@zp1.net>
2024-04-18 19:49:33 +0200

    On branch master Changes to be committed: 	modified:   import_carddav_to_mysql.py

--- import_carddav_to_mysql.py
+++ import_carddav_to_mysql.py
@@ -1,48 +1,78 @@
-import requests
-import xmltodict
+#!/usr/bin/env python3
+
+import json
+import os
 import mysql.connector
+import requests
+from dotenv import load_dotenv
 from mysql.connector import Error
-
-# CardDAV server details
-url = 'https://phoneblock.net/phoneblock/contacts/'
-username = '88289e81-9b63-432e-a6a3-61608298db45'
-password = 'dgB7YSzoEE5TGXSaFZEb'
-
-# MySQL database details
-db_host = 'localhost'
-db_database = 'phoneblock'
-db_user = 'phoneblock'
-db_password = 'phoneblock'
 
 def fetch_carddav_data(url, username, password):
     response = requests.get(url, auth=(username, password))
+    data = json.loads(response.text) 
     if response.status_code == 200:
-        return response.text
+        return data
     else:
         return None
 
+def sql_execute_from_file(filename, cursor):
+    with open(filename, 'r', encoding='utf-8') as sql_file:
+        sql_script = sql_file.read()
+    sql_commands = sql_script.split(';')
+    for command in sql_commands:
+        cursor.execute(command)
+
 def save_to_mysql(data, db_details):
     try:
-        connection = mysql.connector.connect(host=db_details['host'],
-                                             database=db_details['database'],
-                                             user=db_details['user'],
-                                             password=db_details['password'])
+        connection = mysql.connector.connect(
+            host=db_details['host'],
+            database=db_details['database'],
+            user=db_details['user'],
+            password=db_details['password']
+        )
         if connection.is_connected():
             cursor = connection.cursor()
-            cursor.execute("CREATE TABLE IF NOT EXISTS addressbook (id INT AUTO_INCREMENT PRIMARY KEY, content LONGTEXT)")
-            cursor.execute("INSERT INTO addressbook (content) VALUES (%s)", (data,))
+            sql_execute_from_file('create_table.sql', cursor)
+            # Insert or update data
+            insert_update_query = """
+                INSERT INTO addressbook (phone, rating, votes)
+                VALUES (%s, %s, %s)
+                ON DUPLICATE KEY UPDATE
+                    rating = %s,
+                    votes = %s;
+            """
+            for entry in data['numbers']:
+                cursor.execute(
+                    insert_update_query, 
+                    (
+                        entry['phone'], 
+                        entry['rating'], 
+                        entry['votes'], 
+                        entry['rating'],
+                        entry['votes']
+                    )
+                )
             connection.commit()
             cursor.close()
             connection.close()
-    except Error as e:
-        print("Error while connecting to MySQL", e)
+    except mysql.connector.Error as e:
+        print("Error while connecting to MySQL ", e)
 
 def main():
-    carddav_data = fetch_carddav_data(url, username, password)
+    load_dotenv()
+    carddav_data = fetch_carddav_data(
+        os.getenv('CARDDAV_URL'), 
+        os.getenv('CARDDAV_USERNAME'), 
+        os.getenv('CARDDAV_PASSWORD')
+    )
     if carddav_data:
-        xml_data = xmltodict.unparse(xmltodict.parse(carddav_data))
-        db_details = {'host': db_host, 'database': db_database, 'user': db_user, 'password': db_password}
-        save_to_mysql(xml_data, db_details)
+        db_details = {  
+            'host': os.getenv('MYSQL_HOST'), 
+            'database': os.getenv('MYSQL_DATABASE'), 
+            'user': os.getenv('MYSQL_USER'), 
+            'password': os.getenv('MYSQL_PASSWORD')
+        }
+        save_to_mysql(carddav_data, db_details) 
     else:
         print("Failed to fetch data from CardDAV server")
 
